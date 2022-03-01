b21bfdd5524598ab90c5bb28cae411e06471e8d6
ez2rok <69025426+ez2rok@users.noreply.github.com>
2022-03-01 12:37:51 -0500

    Create minimum_number_of_steps_to_make_two_strings_anagram_ii.py

---
+++ week7/contests/leetcode_weekly_282/https:/minimum_number_of_steps_to_make_two_strings_anagram_ii.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        
+        s = Counter(s)
+        t = Counter(t)
+        
+        diff1 = s - t
+        diff2 = t - s
+        
+        n_differences1 = sum(diff1.values())
+        n_differences2 = sum(diff2.values())
+        
+        return n_differences1 + n_differences2
+        
